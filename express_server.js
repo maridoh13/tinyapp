9df75b1d4199555e75c9cb748eb913b36465c324
maridoh13 <maridoh@gmail.com>
2020-02-21 04:13:50 +0000

    all requirements working

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 
 const { getEmailByUserID } = require('./helpers');
@@ -15,7 +15,7 @@ app.use(cookieSession({
   name: 'session',
   keys: ['elephant-key'],
   // maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
 
 
 // DATABASES
@@ -82,30 +82,33 @@ app.get("/urls/new", (req, res) => {
 
 // Creates tiny link
 app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString();
-  let userID = req.session.user_id;
-  urlDatabase[shortURL] = { longURL, userID };
-  res.redirect(`/urls/${shortURL}`);
+  if (!req.session.user_id) {
+    res.send('User not logged in. Cannot create new link.');
+  } else {
+    let longURL = req.body.longURL;
+    let shortURL = generateRandomString();
+    let userID = req.session.user_id;
+    urlDatabase[shortURL] = { longURL, userID };
+    res.redirect(`/urls/${shortURL}`);
+  }
 });
 
 // New tiny link created
 app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL] && req.session.user_id) {
-    res.send('This tiny url is not yours!')
+    res.send('This tiny url is not yours!');
   }
   
   if (!urlDatabase[req.params.shortURL]) {
-    res.send('Tiny url does not exist.')
+    res.send('Tiny url does not exist.');
   }
   
-
   if (!req.session.user_id) {
     res.redirect("/login");
   } else {
     let userID = req.session.user_id;
     let userEmail = getEmailByUserID(userID, users);
-    let shortURL = req.params.shortURL
+    let shortURL = req.params.shortURL;
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL]['longURL'], email: userEmail };
     res.render("urls_show", templateVars);
   }
@@ -113,7 +116,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // Forwarder page
 app.get("/u/:shortURL", (req, res) => {
-    if (!urlDatabase[req.params.shortURL]) {
+  if (!urlDatabase[req.params.shortURL]) {
     res.send("TinyLink does not exist.");
   } else {
     const longURL = urlDatabase[req.params.shortURL]['longURL'];
@@ -132,10 +135,8 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
-// Update a longURL resource - EDIT longURL
+// Update a longURL resource
 app.post("/urls/:shortURL/edit", (req, res) => {
-  let userID = req.session.user_id;
-  let userEmail = getEmailByUserID(userID, users);
   let shortURL = req.params.shortURL;
   urlDatabase[shortURL]['longURL'] = req.body.newlongURL;
   res.redirect(`/urls/${shortURL}`);
@@ -143,7 +144,11 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 
 // Login
 app.get("/login", (req, res) => {
-  res.render("urls_login");
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_login");
+  }
 });
 
 app.post("/login", (req, res) => {
@@ -171,7 +176,11 @@ app.post("/logout", (req, res) => {
 
 // Registration pages
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_register");
+  }
 });
 
 app.post("/register", (req, res) => {
@@ -179,7 +188,7 @@ app.post("/register", (req, res) => {
   let password = req.body.password;
   let hashedPassword = bcrypt.hashSync(password, 10);
   let newUser = { id: newID, email: req.body.email, password: hashedPassword };
-  let reqEmail = req.body.email
+  let reqEmail = req.body.email;
 
   if (req.body.email === '' || req.body.password === '') {
     res.status(400);
@@ -192,8 +201,4 @@ app.post("/register", (req, res) => {
     req.session.user_id = newID;
     res.redirect("/urls");
   }
-});
-
-app.get("/registered", (req, res) => {
-  res.send(users);
 });
