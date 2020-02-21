6b5800d44f3b085837bcef81cb0db2098f3d5401
maridoh13 <maridoh@gmail.com>
2020-02-21 04:19:02 +0000

    final tweaks

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -3,26 +3,26 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+describe('getUserByEmail', () => {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     assert.equal(user, expectedOutput);
   });
 
-  it('should return undefined if no email is found', function() {
-    const user = getUserByEmail("user3@example.com", testUsers)
+  it('should return undefined if no email is found', () => {
+    const user = getUserByEmail("user3@example.com", testUsers);
     assert.equal(user, undefined);
   });
 
